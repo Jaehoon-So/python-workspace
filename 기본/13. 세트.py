4d459a7b587572f91968a174b9642c7ca45509e3
CowJaehoon <76734067+Jaehoon-So@users.noreply.github.com>
2021-03-02 01:45:57 +0900

    Update 13. 세트.py
    맞춤법 수정

--- 기본/13. 세트.py
+++ 기본/13. 세트.py
@@ -6,7 +6,7 @@ print(my_set) # 중복을 허용하지 않기때문에 {1, 2, 3}
 java = {"유재석", "김태호", "양세형"}
 python = set(["유재석", "박명수"])
 
-# 교집합 (java와 python을 보두 할 수 있는 개발자)
+# 교집합 (java와 python을 모두 할 수 있는 개발자)
 print(java & python)
 print(java.intersection(python))
 
@@ -15,15 +15,15 @@ print(java | python)
 print(java.union(python))
 # 순서는 보장되지 않는다
 
-# 차집합 (java 할 수 잇지만 python 은 할 줄 모르는 개발자)
+# 차집합 (java 할 수 있지만 python 은 할 줄 모르는 개발자)
 print(java - python)
 print(java.difference(python))
 
-# python 할줄 아는 사람이 늘어남
+# python 할 줄 아는 사람이 늘어남
 python.add("김태호")
-print(python) #김태호 씨도 들어간 모습을 확인 할 수 있다.
+print(python) # 김태호씨도 들어간 모습을 확인 할 수 있다.
 
-# java 를 잊었어요
+# java를 잊었어요
 java.remove("김태호")
 print(java)
 
